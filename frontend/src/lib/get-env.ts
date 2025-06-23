455d051eb3a91a6b6b4a1fc0d010775132402f51
Arpit Kushwaha <arpit@example.com>
2025-06-23 20:06:11 +0530

    Updated project files

--- frontend/src/lib/get-env.ts
+++ frontend/src/lib/get-env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://calendar-scheduling-6.onrender.com/api",
   VITE_APP_ORIGIN: import.meta.env.VITE_APP_ORIGIN || "http://localhost:5175",
 };
